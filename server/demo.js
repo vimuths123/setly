d0eb5ed778aae614f4f67bc362e0239805ca4e42
vimuths123 <vimuths@yahoo.com>
2023-04-20 18:46:21 +0530

    Change

--- server/demo.js
+++ server/demo.js
@@ -72,6 +72,7 @@ exports.handler = async (event, context) => {
       headers: { Location: '/calender.html' }
     }
   } catch (error) {
+    console.log('hete')
     // console.error(error);
     // console.error(error);
     mongoose.disconnect();
